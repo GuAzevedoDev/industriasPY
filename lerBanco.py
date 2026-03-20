358d78cbb34535ef831f021dfcbc6e59a3d5a048
Gustavo Azevedo <gustavoazevedo.code@gmail.com>
2026-03-20 18:57:22 -0300

    buscar peca

--- lerBanco.py
+++ lerBanco.py
@@ -1,6 +1,6 @@
 def lerBanco():
     import json
-    caminho = r"C:\Users\Gustavo\Documents\Python\industriasPY\bancoDeDados.json"
+    caminho = r"C:\Users\Gustavo\Documents\Python\industriasPY\banco.json"
     with open(caminho,"r", encoding="utf-8") as arquivo: 
         dados = json.load(arquivo)
     return dados
--- mostrarBanco.py
+++ mostrarBanco.py
@@ -10,15 +10,36 @@ def mostrar_banco():
         print(f"Peca: {peca}")
 
 
-def buscar_peca():
-    pecas = lerBanco()["pecas"]
-    pecaBuscada = "Modulo telemetria TX/RX"
-    for peca in pecas:
-        if pecaBuscada == peca["peca"]:
-            chave = peca["id"]
-            peca = peca["peca"]
-            print(f"ID: {chave}")
-            print(f"Peca: {peca}")
 
-            
+def buscar_peca(pecaBuscada):
+    encontrada = False
+    encontrados = []
+    pecas = lerBanco()['pecas']
+    for peca in pecas:
+        if pecaBuscada.lower() in peca['peca'].lower():
+            encontrada = True
+            encontrados.append(peca['id'])
+            print(f"ID: {peca['id']}")
+            print(f"Peca: {peca['peca']}\n")
+    if len(encontrados) > 1:
+        print(f"\n\nForam encontradas {len(encontrados)} pecas que correspondem a sua pesquisa\n")
+        idInput = int(input("Digite o id da peca escolhida: "))
+        if idInput in encontrados:
+            for peca in pecas:
+                if idInput == peca['id']:
+                    print(f"ID: {peca['id']}")
+                    print(f"Peca: {peca['peca']}\n")
+        else: print("O id digitado nao esta na lista")
+
+    if not encontrada:
+        print("Peca nao encontrada")   
+        
+buscar_peca("Volante esportivo")
+
+
+
+                     
+
     
+
+
