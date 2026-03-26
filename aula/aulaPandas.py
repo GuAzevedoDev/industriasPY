b72ed3c00511f4238e617e288e2eec52c13222e1
ARTHUR_FERREIRA_RAYMUNDO <22401768@DELTAF22.FLORESTA>
2026-03-26 08:54:53 -0300

    aula pandas

--- aula/aulaPandas.py
+++ aula/aulaPandas.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
-caminho = r"H:\industriasPY\aula\banco_teste - banco_teste - banco_teste - banco_teste.csv"
+caminho = r"H:\3A2 Python\industriasPY\aula\banco_teste - banco_teste - banco_teste - banco_teste.csv"
 
-df = pd.read_csv(caminho)
+df = pd.read_csv(caminho, sep=";")
 
-print(df)
+
+# print(df.head())
+
+# print(df.dtypes)
+
+
+df['pais'] = "Brasil"
+
+
+print(df.head())
